777f5f2273da7d4d2989c6bda3bd587a257fbd0d
Alisher Askar <alisher.askar.work@gmail.com>
2026-06-24 19:07:43 +0500

    feat: add react-router & configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Home } from './pages/Home';
+import { Inner } from './pages/Inner';
+
 export const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/inner'
+					element={<Inner />}
+				/>
+				<Route
+					path='*'
+					element={<Home />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 };
---
+++ src/pages/Inner.tsx
@@ -0,0 +1,3 @@
+export const Inner = () => {
+	return <div>Inner</div>;
+};
