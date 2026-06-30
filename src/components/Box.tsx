ef64867740d211e15e70d6a98f6148c399cb2de4
Alisher Askar <alisher.askar.work@gmail.com>
2026-06-30 13:26:18 +0500

    feat(Box): accept section props

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,13 +1,14 @@
-import type { ReactNode } from 'react';
+import { cn } from '@/utils/cn';
+import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
-interface BoxProps {
+interface BoxProps extends ComponentPropsWithoutRef<'section'> {
   title: string;
   children: ReactNode;
 }
 
-export const Box = ({ title, children }: BoxProps) => {
+export const Box = ({ title, children, className, ...props }: BoxProps) => {
   return (
-    <section className="mb-12">
+    <section className={cn('mb-12', className)} {...props}>
       <h2 className="text-primary mb-10 text-2xl font-bold">{title}</h2>
       <div>{children}</div>
     </section>
